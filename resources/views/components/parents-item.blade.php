662b63fcbe28bebb928973c6cc476a0411414035
amedwishpond <83128608+amedwishpond@users.noreply.github.com>
2023-06-21 10:58:01 -0600

    updtes

---
+++ resources/views/components/parents-item.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <div>
+        <div class="grid grid-cols-2 gap-10">
+            <div class="flex flex-col items-center">
+                <x-rounded-default-img
+                    src="/img/demo-hero.png"
+                />
+                <p class="mt-4 text=[#152F4C] font-semibold">{{ $he }}</p>
+            </div>
+            <div class="flex flex-col items-center">
+                <x-rounded-default-img
+                    src="/img/demo-hero.png"
+                />
+                <p class="mt-4 text=[#152F4C] font-semibold">{{ $she }}</p>
+            </div>
+        </div>
+        <h4 class="text-center mt-6 text-lg text-[#D28A55]">{{ $title }}</h4>
+    </div>
+</div>
